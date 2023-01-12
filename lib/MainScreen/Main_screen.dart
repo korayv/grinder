c5d75613512bd0aa934787935761c6839454b0b4
FATİH TALHA YAVUZASLANOĞLU <95251900+fatihTalhaYavuz@users.noreply.github.com>
2023-01-12 19:46:45 +0300

    app icon and name changed

--- lib/MainScreen/Main_screen.dart
+++ lib/MainScreen/Main_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sa/Create%20Screen/Create_Screen.dart';
 import 'package:sa/Drawer/get_premium.dart';
-import 'package:sa/Drawer/privacy_policy.dart';
-import 'package:sa/Drawer/rate_us.dart';
-import 'package:sa/Drawer/term_of_use.dart';
 import 'package:sa/MainScreen/NFTScreens.dart';
 import 'package:sa/MainScreen/about_us.dart';
 import 'package:sa/MainScreen/open_wallet.dart';
@@ -12,8 +9,8 @@ import 'package:sa/MainScreen/profile_screen.dart';
 import 'package:sa/views/remove_bg_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../Drawer/Follow_us.dart';
 import '../imageEdit/editScreen.dart';
+import 'main2.dart';
 
 final Uri _url = Uri.parse('https://foundation.app/@philipglass/BM-ICONS/1');
 final Uri _url2 = Uri.parse('https://foundation.app/@Timpers/foundation/28521');
@@ -91,98 +88,7 @@ class _MyAppState extends State<MainScreen> {
                         context,
                         MaterialPageRoute(
                           builder: (context) {
-                            return Drawer(
-                              child: SafeArea(
-                                  child: Column(
-                                children: [
-                                  ListTile(
-                                    dense: true,
-                                    title: Text("Rate Us"),
-                                    leading: Icon(Icons.star_border_outlined),
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) => RateUs(),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  ListTile(
-                                    dense: true,
-                                    title: Text("Privacy Policy"),
-                                    leading: Icon(Icons.privacy_tip_outlined),
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) => PravicyPolicy(),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  ListTile(
-                                    dense: true,
-                                    title: Text("Terms of Use"),
-                                    leading: Icon(Icons.article_outlined),
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) => TermOfUse(),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  ListTile(
-                                    dense: true,
-                                    title: Text("Follow Us"),
-                                    leading: Icon(Icons.thumb_up_alt_outlined),
-                                    onTap: () {
-                                      Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) {
-                                            return ContactUs(
-                                              cardColor: Colors.white,
-                                              companyColor: Colors.lime,
-                                              companyName: 'GriNder',
-                                              email: 'grinder@gmail.com',
-                                              taglineColor: Colors.lime,
-                                              textColor: Colors.lime,
-                                              logo: AssetImage(
-                                                  'lib/images/download.jpg'),
-                                              website: 'griNder.com',
-                                              avatarRadius: 100,
-                                              companyFontWeight:
-                                                  FontWeight.bold,
-                                              instagram: 'griNder',
-                                              facebookHandle: 'grinder',
-                                              linkedinURL: 'grinder',
-                                              phoneNumber: '0555 555 55 55',
-                                              dividerColor: Colors.brown,
-                                              companyFontSize: 50,
-                                              twitterHandle: 'GriNder',
-                                            );
-                                          },
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  ListTile(
-                                    dense: true,
-                                    title: Text("Go Back"),
-                                    leading:
-                                        Icon(Icons.arrow_back_ios_outlined),
-                                    onTap: () => Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) {
-                                          return MainScreen();
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )),
-                            );
+                            return mainpage2();
                           },
                         ),
                       ),
